748da8b46055bea064a34d0b57f674dbc273e583
Chintan <itzchintan14@gmail.com>
2021-07-28 02:36:29 +0530

    navbar created

---
+++ src/components/Navbar/Elements.js
@@ -0,0 +1,27 @@
+export const Elements = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Movies',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Plays',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Contact Us',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Contact Us',
+        url: '#',
+        cName: 'nav-links-mobile'
+    },
+]
