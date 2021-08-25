d321115cdd2dc6c010ece0b39eae2b3bde36da88
kunju004 <kunjpatel3103@gmail.com>
2021-08-26 03:21:28 +0530

    changes made in card comp.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import React,{Component} from 'react';
 import Navbar from "./components/Navbar/Navbar";
 import Cards from "./components/Cards";
+import "bootstrap/dist/css/bootstrap.min.css";
 class App extends Component {
   render(){
     return (
       <div className="App">
-        <Navbar />  
+        <Navbar /> 
+        
       </div>
     );
   }
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,15 +1,41 @@
 import React from 'react';
-
+import './Card.css';
 function Cards(){
     return (
-    <div class="card">
-    <img src="https://www.google.com/search?q=image&rlz=1C1UEAD_enIN964IN964&sxsrf=ALeKk02BXEjb1w5xYoLzYyG6KlPDd5ZQBA:1629235005034&tbm=isch&source=iu&ictx=1&fir=gxFxsvFBmxeZ9M%252C0JWe7yDOKrVFAM%252C_&vet=1&usg=AI4_-kReDzFRmzDAOj_DsRSOvMu2NwesqQ&sa=X&ved=2ahUKEwiF1-ut_bjyAhUQ63MBHQKjBE4Q9QF6BAgPEAE#imgrc=gxFxsvFBmxeZ9M" class="card-img-top" alt="..."/>
+    
+      <div class="row row-cols-1 row-cols-md-2 g-4">
+      <div class="col">
+        <div class="card">
+    <div class="card-header">
+      <h2>Movie-name</h2>
+      </div>
+      <img src="https://reactjsexample.com/content/images/2020/01/React-MovieCards-1.jpg" class="card-thumbnail" alt=""/>
     <div class="card-body">
-      <h5 class="title">Card title</h5>
       <p className="text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
+      
+      <div class="d-grid gap-2 col-4 mx-auto">
+      <button type="button">Book</button>
+      </div>
+      </div>
+  </div>
+  </div>
+
+  <div class="col">
+        <div class="card">
+    <div class="card-header">
+      <h2>Movie-name</h2>
+      </div>
+      
+      <img src="https://reactjsexample.com/content/images/2020/01/React-MovieCards-1.jpg" class="img-thumbnail" alt=""/>
+    <div class="card-body">
+      <p className="text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <div class="d-grid gap-2 col-4 mx-auto">
+      <button type="button">Book</button>
+      </div>
     </div>
   </div>
+  </div>
+      </div>
     );
 }
 
