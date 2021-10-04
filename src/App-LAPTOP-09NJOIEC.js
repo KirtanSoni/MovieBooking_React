befec31b8f0f2d3acb105bc7b4528a83ee3b38c0
kunju004 <kunjpatel3103@gmail.com>
2021-10-04 20:31:26 +0530

    movie structure

--- src/App-LAPTOP-09NJOIEC.js
+++ src/App-LAPTOP-09NJOIEC.js
@@ -6,19 +6,17 @@ import { Route } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
 import Movies from './components/Movies';
 import { Fragment } from 'react/cjs/react.production.min';
-import Demo from './components/demo';
-import Login from './components/Forms/login';
+
 
 class App extends Component {
   render(){
     return (
-      // <Login />
       <Fragment>
       <Navbar/>
-      {/* <Demo /> */}
       <Movies/>
         <BrowserRouter>
             <switch>
+              <Route exact path="/navbar" component={Navbar} />
               <Route exact path="/Movies" component={Movies} />
             </switch>
         </BrowserRouter>
---
+++ src/components/Movies/Movies.js
@@ -0,0 +1,38 @@
+import { render } from '@testing-library/react';
+import React, {Component} from 'react';
+import Cards from '../cards/Card';
+import './Movies.css';
+
+
+class Movies extends Component{
+
+    render(){
+    return (
+    
+           <div className="container-fluid d-flex justify-content-center">
+               <div className="row">
+                   <div className="col-md-4">
+                       <Cards title="movie 1" imgsrc="https://reactjsexample.com/content/images/2020/01/React-MovieCards-1.jpg" class="card-thumbnail" alt="" content="Some quick example text to build on the card title and make up the bulk of the card's content"/>
+                   </div>
+                   <div className="col-md-4">
+                   <Cards title="movie 3" imgsrc="https://reactjsexample.com/content/images/2020/01/React-MovieCards-1.jpg" class="card-thumbnail" alt="" content="Some quick example text to build on the card title and make up the bulk of the card's content"/>
+                   </div>
+                   <div className="col-md-4">
+                   <Cards title="movie 3" imgsrc="https://reactjsexample.com/content/images/2020/01/React-MovieCards-1.jpg" class="card-thumbnail" alt="" content="Some quick example text to build on the card title and make up the bulk of the card's content"/>
+                   </div>
+                   <div className="col-md-4">
+                   <Cards title="movie 3" imgsrc="https://reactjsexample.com/content/images/2020/01/React-MovieCards-1.jpg" class="card-thumbnail" alt="" content="Some quick example text to build on the card title and make up the bulk of the card's content"/>
+                   </div>
+                   <div className="col-md-4">
+                   <Cards title="movie 3" imgsrc="https://reactjsexample.com/content/images/2020/01/React-MovieCards-1.jpg" class="card-thumbnail" alt="" content="Some quick example text to build on the card title and make up the bulk of the card's content"/>
+                   </div>
+                   <div className="col-md-4">
+                   <Cards title="movie 3" imgsrc="https://reactjsexample.com/content/images/2020/01/React-MovieCards-1.jpg" class="card-thumbnail" alt="" content="Some quick example text to build on the card title and make up the bulk of the card's content"/>
+                   </div>
+               </div>
+           </div>
+      
+    );
+}
+}
+export default Movies;
