dd57f2f8a0ec27c8066e667aa5d57a375aa21861
kunju004 <kunjpatel3103@gmail.com>
2021-10-22 14:38:16 +0530

    Contact page stylings done

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-
-const ContactUs=()=>{
+import phn from './Forms/phn.png'
+import mail from './Forms/mail.png'
+import add from './Forms/add.png'
+function ContactUs(){
     
-    return (
-        <>
-        
-    <div className="container">
+    return <div className="ContactUs">
+    <div className="container_1">
         <div className="container-fluid">
             <div className="row">
                 <div className="col-lg-10 offset-lg-1 d-flex justify-content-between">
-                    <div className="contact_info_item d-flex justify-content-start align-items-center">
-                        <img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.pngitem.com%2Fpimgs%2Fm%2F64-641423_blue-phone-logo-png-transparent-blue-phone-icon.png&imgrefurl=https%3A%2F%2Fwww.pngitem.com%2Fmiddle%2FbbTbRm_blue-phone-logo-png-transparent-blue-phone-icon%2F&tbnid=jdk5W9w4UBU5rM&vet=12ahUKEwi_1Lyf99jzAhVc13MBHXpMDd4QMygKegUIARDdAQ..i&docid=gUZ353HLOoi84M&w=860&h=708&q=phone%20logo%20png&ved=2ahUKEwi_1Lyf99jzAhVc13MBHXpMDd4QMygKegUIARDdAQ" alt="phone" />
+                    <div className="contact_info_phn d-flex justify-content-start align-items-center">
+                    <img src={phn} height="50" alt="phn" />
                         <div className="contact_info_content">
                         <div className="contact_info_title">
                             Phone
@@ -21,7 +21,7 @@ const ContactUs=()=>{
                         </div>
                     </div>   
                     <div className="contact_info_item d-flex justify-content-start align-items-center">
-                        <img src="" alt="Email" />
+                        <img src={mail} height="50" alt="Email" />
                         <div className="contact_info_content">
                         <div className="contact_info_title">
                        Email
@@ -33,7 +33,7 @@ const ContactUs=()=>{
                     </div>  
 
                     <div className="contact_info_item d-flex justify-content-start align-items-center">
-                        <img src="" alt="Address" />
+                        <img src={add} height="50"  alt="Address" />
                         <div className="contact_info_content">
                         <div className="contact_info_title">
                             Adrress
@@ -54,9 +54,9 @@ const ContactUs=()=>{
                 <div className="col-lg-10 offset-lg-1">
                     <div className="contact_form_container py-5">
                         <div className="contact_form_title">
-                            Connect with Us
+                            <b>Connect with Us</b>
                         </div>
-                        <form id="contact_form">
+                        <form id="contact_form_1">
                             <div className="contact_form_name d-flex justify-content-between align-items-between">
                                 <input type="text" id="contact_form_name" className="contact_form_name input_field" placeholder="Your name" required="true">
 
@@ -85,8 +85,6 @@ const ContactUs=()=>{
             </div>
         </div>
     </div>
-    
-    </>
-    )
+    </div>
 }
 export default ContactUs;
