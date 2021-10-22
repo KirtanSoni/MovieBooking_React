6bd5d6059bf07d4185e5d79e6d87d9030ebea60e
kunju004 <kunjpatel3103@gmail.com>
2021-10-22 21:22:17 +0530

    minor updte

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Home(){
     return <div className="container">
-        <h6>design Home page in /components/Home.jsx add something like city selection to view available plays and movies in your city</h6>
+       
     </div>
 }
 export default Home;
