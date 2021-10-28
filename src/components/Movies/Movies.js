b6afd535fc50b4171b7b092558222aac9468b421
Chintan <itzchintan14@gmail.com>
2021-10-29 02:12:09 +0530

    for loop

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -10,18 +10,16 @@ import { useSelector } from 'react-redux';
 const Movies = () =>{
     const check=useAuth();
     const user = useSelector((state) => state.user);
+    let items=['Item 1','Item 2','Item 3','Item 4','Item 5'];
     console.log(check);
     console.log(user);
     return (<div>
         {Object.keys(user).length !=0?<div>
             <div className="container-fluid d-flex justify-content-center">
             <div className="row">
-                <div className="col-md-4">
-                    <Cards title="movie 1" id="1" imgsrc="https://reactjsexample.com/content/images/2020/01/React-MovieCards-1.jpg" class="card-thumbnail" alt="" content="Some quick example text to build on the card title and make up the bulk of the card's content"/>
-                </div>
-            <div className="col-md-4">
-                <Cards title="movie 2" id="2" imgsrc="https://reactjsexample.com/content/images/2020/01/React-MovieCards-1.jpg" class="card-thumbnail" alt="" content="Some quick example text to build on the card title and make up the bulk of the card's content"/>
-            </div>
+            {items.map((item,index)=>{
+                return <div key={index} className="col-md-4"><Cards title={item} id="1" imgsrc="https://reactjsexample.com/content/images/2020/01/React-MovieCards-1.jpg" class="card-thumbnail" alt="" content="Some quick example text to build on the card title and make up the bulk of the card's content"/></div>
+            })}
         </div>
     </div>
         </div>:<Redirect to={{
